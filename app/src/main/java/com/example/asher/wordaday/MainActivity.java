03429a7a22c20b5c3a4b447f660379e83e998cd1
Asherx15 <asher.faisal@gmail.com>
2018-08-30 17:33:38 -0400

    First commit. Done with UI

---
+++ app/src/main/java/com/example/asher/wordaday/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.asher.wordaday;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void pl (View view){
+        Intent intent= new Intent(getApplicationContext(), Main2Activity.class);
+        startActivity(intent);
+    }
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+}
